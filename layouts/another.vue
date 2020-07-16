42f42b7fc0f21679659bd56437943a13f0fbc99c
SimonWT <y.yudinskikh@innopolis.ru>
2020-07-16 12:00:48 +0400

    test redirect

---
+++ layouts/another.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/redirect/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    Redirecting...
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'another',
+  name: 'Redirect',
+  mounted() {
+    console.log(
+      'phone',
+      this.getQueryVariable('phone', this.$route.query.whatsup)
+    )
+    window.location.href =
+      'https://api.whatsapp.com/send?' + this.$route.query.whatsup // phone=79191646801&text=Здравствуйте,%20Подскажите%20цену%20цветов'
+  },
+  methods: {
+    getQueryVariable(variable, query) {
+      const vars = query.split('&')
+      for (let i = 0; i < vars.length; i++) {
+        const pair = vars[i].split('=')
+        if (decodeURIComponent(pair[0]) === variable) {
+          return decodeURIComponent(pair[1])
+        }
+      }
+      console.log('Query variable %s not found', variable)
+    },
+  },
+}
+</script>
